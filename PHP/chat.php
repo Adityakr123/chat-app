d337a612c8fabc2ec55eb4b4706c9f0ba2cdf6de
Aditya Kumar <adityakr365@gmail.com>
2022-03-02 14:47:56 +0530

    adding

--- PHP/chat.php
+++ PHP/chat.php
@@ -19,17 +19,19 @@ if(isset($_POST["username"])){
     $Username = $_POST['username'];
     $table=$fromname.$Username;
     echo"$table";
-    $conn->query("CREATE TABLE $table (sender varchar(255),reciever varchar(255),message varchar(255),DATE )");
+    $conn->query("CREATE TABLE $table (sender varchar(255),reciever varchar(255),message varchar(255),DATE DATETIME)");
+    // $conn->query("INSERT INTO $table(sender,reciever,message,DATE) VALUES ('A', 'A', 'A', current_timestamp());");
+    
     $sql = "SELECT Username,password FROM login where Username='$Username'";
     $result = $conn->query($sql);
-    // if (!empty($result) && $result->num_rows > 0) {
-    //     while($row = mysqli_fetch_assoc($result)) 
-    //     {
+    if (!empty($result) && $result->num_rows > 0) {
+        while($row = mysqli_fetch_assoc($result)) 
+        {
            
               
            
-    //     }
-    // }
+        }
+    }
 
 }
 ?>
